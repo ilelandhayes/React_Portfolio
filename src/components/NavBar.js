5e33d76606da8d17f063bc132d0b3cf14a2338be
Leland Hayes <lelandkhayes13@gmail.com>
2021-10-23 14:11:51 -0400

    got navbar centered, and updated ui for home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,68 +5,83 @@ import "./styles/NavBar.css";
 
 const NavBar = ({ currentPage, handlePageChange }) => {
   return (
-    <Router>
-      <Container className="justify-content-center">
-      <Navbar className="justify-content-center px-3" variant="dark" expand="lg" sticky="top">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse
-          // id="basic-navbar-nav"
-          // // className="float-to-right"
-        ></Navbar.Collapse>
-        <Nav className="justify-content-center" activeKey="/home">
-          <Nav.Item className="justify-content-center">
-            <Nav.Link
-              className={
-                currentPage === "Home" ? "nav-link active" : "nav-link"
-              }
-              onClick={() => handlePageChange("Home")}
+    <div className="row">
+      <div className="col-md-12">
+        <Router>
+          <Container className="nav1">
+            <Navbar
+              className="nav1 justify-content-center px-3"
+              variant="dark"
+              expand="lg"
+              sticky="top"
             >
-              Home
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              className={
-                currentPage === "AboutMe" ? "active active-page-link" : "active"
-              }
-              onClick={() => handlePageChange("AboutMe")}
-            >
-              About Me
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              className={
-                currentPage === "Work" ? "active active-page-link" : "active"
-              }
-              onClick={() => handlePageChange("Work")}
-            >
-              Work
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              className={
-                currentPage === "Contact" ? "active active-page-link" : "active"
-              }
-              onClick={() => handlePageChange("Contact")}
-            >
-              Contact Me
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link
-              className="active"
-              href="assets/resume/Leland_Hayes_Resume (2).PDF"
-              download
-            >
-              Resume
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar>
-      </Container>
-    </Router>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse
+              // id="basic-navbar-nav"
+              // // className="float-to-right"
+              ></Navbar.Collapse>
+              <Nav className="justify-content-center" activeKey="/home">
+                <Nav.Item className="justify-content-center">
+                  <Nav.Link
+                    className={
+                      currentPage === "Home" ? "nav-link active" : "nav-link"
+                    }
+                    onClick={() => handlePageChange("Home")}
+                  >
+                    Home
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link
+                    className={
+                      currentPage === "AboutMe"
+                        ? "active active-page-link"
+                        : "active"
+                    }
+                    onClick={() => handlePageChange("AboutMe")}
+                  >
+                    About Me
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link
+                    className={
+                      currentPage === "Work"
+                        ? "active active-page-link"
+                        : "active"
+                    }
+                    onClick={() => handlePageChange("Work")}
+                  >
+                    Work
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link
+                    className={
+                      currentPage === "Contact"
+                        ? "active active-page-link"
+                        : "active"
+                    }
+                    onClick={() => handlePageChange("Contact")}
+                  >
+                    Contact Me
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link
+                    className="active"
+                    href="assets/resume/Leland_Hayes_Resume (2).PDF"
+                    download
+                  >
+                    Resume
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar>
+          </Container>
+        </Router>
+      </div>
+    </div>
   );
 };
 
