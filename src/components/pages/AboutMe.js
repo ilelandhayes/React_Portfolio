cd3a407c690bcf57e155d640acfb149685344047
Leland Hayes <lelandkhayes13@gmail.com>
2021-10-24 17:43:05 -0400

    added border in contact page, and updated spelling on about page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -10,7 +10,7 @@ const AboutMe = ({ handlePageChange }) => {
         <h2 className="about-section my-5 divEl">
           Being involved in technology has been an on going fascination of mine. It has always
           been more about knowing how the technology runs/works instead of just playing what the 
-          developer made. What really got me into web development was always scrolling through
+          developer made. What really got me into web development was scrolling through
           the internet and coming across these websites that were very intriguing and well coded. 
           After watching a few video's on it, I decided to enroll in a coding bootcamp at UNC
           Chapel Hill (the best decision of my life). From there I started my coding career as
